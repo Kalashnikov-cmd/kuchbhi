26606c4a62012c6375a3886eb8aa33e75993a7a4
Lohit Paul <marskov77@gmail.com>
2020-11-12 12:02:33 +0530

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyCuFw__ZlFxQVK9lYEy776Oliflln9XLIQ",
+    authDomain: "smartkart-cb302.firebaseapp.com",
+    databaseURL: "https://smartkart-cb302.firebaseio.com",
+    projectId: "smartkart-cb302",
+    storageBucket: "smartkart-cb302.appspot.com",
+    messagingSenderId: "585949540632",
+    appId: "1:585949540632:web:01be0e8c88f6706f85ee6d",
+    measurementId: "G-YJN4BF68BR"
+    }
+  };
